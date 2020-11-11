a9982a74b06ef1f0897f67db44422a15848b9cc0
Donald Chromy <dgchromy@gmail.comglobal user.emailgit config user.emailgit config --global user.email dgchromy@gmail.com>
2020-11-10 19:41:46 -0800

    second heroku push

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect("mongodb://localhost/STM", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
